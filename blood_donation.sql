375e0975b1403d1f83faa21c7279f2be9e8746e3
Man-Thakkar03 <manthakkar2005@gmail.com>
2025-05-17 11:04:42 +0530

    Update blood_donation.sql

--- blood_donation.sql
+++ blood_donation.sql
@@ -17,3 +17,10 @@ CREATE TABLE notifications (
     email VARCHAR(100) UNIQUE,
     subscribed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE contact_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
